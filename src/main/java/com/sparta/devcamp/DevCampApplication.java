b593db714cd2ca3ecd04a28aa96e5299e21834e3
mingang211 <a989977@naver.com>
2024-03-19 00:00:08 +0900

    init commit

---
+++ src/main/java/com/sparta/devcamp/DevCampApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.devcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/devcamp/DevCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.devcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
